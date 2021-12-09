ab5353ed8e04424230057d7887048a9d53c5d169
iHoHyeon <hyunee169@naver.com>
2021-12-10 02:39:29 +0900

    프로그래머스 level1 같은 숫자는 싫어

---
+++ programmers/level1/같은 숫자는 싫어.js
@@ -0,0 +1,9 @@
+// https://programmers.co.kr/learn/courses/30/lessons/12906
+
+const solution = (a) => a.filter((v, i) => v !== a[--i]);
+
+/*
+  부스트캠프 챌린지 첫 날에 고차함수를 보고 충격받았던 기억이..
+
+  비슷한 문제라서 무난하게 풀 수 있었다
+*/
