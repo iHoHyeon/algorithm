8ff8c59cd9f1f050fc0820f2615d860639d745a3
iHoHyeon <hyunee169@naver.com>
2021-12-27 13:45:24 +0900

    백준 silver 11053 - 가장 긴 증가하는 부분 수열

---
+++ baekjoon/다이나믹 프로그래밍 (silver)/11053.cpp
@@ -0,0 +1,37 @@
+// https://www.acmicpc.net/problem/11053
+
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, A[1001], dp[1001], res = 1;
+    fill_n(dp, 1001, 0);
+    cin >> N;
+    cin >> A[1];
+    dp[1] = 1;
+
+    for (int i = 2; i <= N; i++) {
+        cin >> A[i];
+
+        for (int j = 1; j < i; j++) {
+            if (A[j] < A[i]) dp[i] = max(dp[j], dp[i]);
+        }
+        dp[i]++;
+        res = max(res, dp[i]);
+    }
+
+    cout << res;
+
+    return 0;
+}
+
+/*
+  수열 A를 입력 받으면서 이전의 최대 증가 수열의 dp를 저장해놓는다.
+*/
